4586061273b09b1efd64052b54c5ea9a7868b0da
SShrestha <sandipsth@hotmail.com>
2015-10-11 19:12:11 +1100

    User Feed - Leave Comment

---
+++ app/src/main/java/fanx/instl/activity/UserFeedActivity.java
@@ -0,0 +1,36 @@
+package fanx.instl.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import fanx.instl.R;
+import fanx.instl.activity.InstagramUtils.InstagramMediaCommentTask;
+
+public class UserFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_feed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Sample Call
+        //Replace 1092484232200772082_2219605693 -> Appropriate Media
+        InstagramMediaCommentTask c  = new InstagramMediaCommentTask("1092484232200772082_2219605693", getApplicationContext());
+        c.execute("This is a test comment");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
